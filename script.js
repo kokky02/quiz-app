5fec5447c145b7f9773dfa7a30618b64268c75ef
Kokky02 <daniel.kokay@gmail.com>
2023-10-21 14:48:36 +0200

    completing function for show questions

--- script.js
+++ script.js
@@ -82,6 +82,13 @@ function showQuestion(){
      let currentQuestion = questions[currentQuestionIndex]
      let questionNo = currentQuestionIndex + 1
      questionElement.innerHTML = questionNo + '. ' + currentQuestion.question
+
+     currentQuestion.answers.forEach(answer => {
+          const button = document.createElement('button')
+          button.innerHTML = answer.text
+          button.classList.add('btn')
+          answerButton.appendChild(button)
+     })
 }
 
 
