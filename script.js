ced51233fede1900e433e355e2a84fcb1afa10b2
Kokky02 <daniel.kokay@gmail.com>
2023-10-21 14:36:02 +0200

    created questions and answers in JS

---
+++ script.js
@@ -0,0 +1,65 @@
+const questions = [
+     {
+          question: 'Jakou hůlku měl Albus Brumbál?',
+          answers: [
+               {text: 'Bezovou', correct: true},
+               {text: 'Neznámou', correct: false},
+               {text: 'Habrovou', correct: false},
+               {text: 'Cesmínovou', correct: false},
+          ]
+     },
+     {
+          question: 'Která z uvedených NENÍ relikvie smrti?',
+          answers: [
+               {text: 'Neviditelný plášť', correct: false},
+               {text: 'Bezedný kotlík', correct: true},
+               {text: 'Kámen vskříšení', correct: false},
+               {text: 'Bezová hůlka', correct: false},
+          ]
+     },
+     {
+          question: 'Kdo byl nejstarší??',
+          answers: [
+               {text: 'Harry Potter', correct: false},
+               {text: 'Draco Malfoy', correct: false},
+               {text: 'Ren Weasley', correct: false},
+               {text: 'Hermiona Grangerová', correct: true},
+          ]
+     },
+     {
+          question: 'Od koho dostal Rubeus Hagrid létající motorku?',
+          answers: [
+               {text: 'Od Siriuse Blacka', correct: true},
+               {text: 'Od Albuse Brumbála', correct: false},
+               {text: 'Od Jamese Pottera', correct: false},
+               {text: 'Od Arthura Weasleho', correct: false},
+          ]
+     },
+     {
+          question: 'Jaké je číslo trezoru Harryho Pottera?',
+          answers: [
+               {text: '713', correct: false},
+               {text: '711', correct: false},
+               {text: '687', correct: true},
+               {text: '998', correct: false},
+          ]
+     },
+     {
+          question: 'Jak se jmenovala Lenka celým jménem?',
+          answers: [
+               {text: 'Lenka Střelenka', correct: false},
+               {text: 'Lenka Láskorádová', correct: true},
+               {text: 'Lenka Laskavá', correct: false},
+               {text: 'Lenka Lákavá', correct: false},
+          ]
+     },
+     {
+          question: 'Jak se jmenoval třihlavý pes, který hlídal vchod ke kameni mudrců?',
+          answers: [
+               {text: 'Tesák', correct: false},
+               {text: 'Dráp', correct: false},
+               {text: 'Norbert', correct: false},
+               {text: 'Chloupek', correct: true},
+          ]
+     }
+];
